e54d892020b2760f069a6b7880c1b2bf4560186f
Rohit Tigga <rrtigga@ucdavis.edu>
2014-05-18 04:16:29 -0700

    c
    c

---
+++ Emote/src/main/java/com/davishacks/emote/Mood_Record_Activity.java
@@ -0,0 +1,27 @@
+package com.davishacks.emote;
+
+import android.os.Bundle;
+
+/**
+ * Created by Spicycurryman on 5/18/14.
+ */
+public class Mood_Record_Activity extends Select_Intensity_Activity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.mood_recorded);
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
