ee9480ee009ec17c5d2b9a8a8d257134bfd677dd
Rohit Tigga <rrtigga@ucdavis.edu>
2014-05-18 02:33:24 -0700

    d
    d

---
+++ Emote/src/main/java/com/davishacks/emote/Select_Intensity_Activity.java
@@ -0,0 +1,22 @@
+package com.davishacks.emote;
+
+import android.os.Bundle;
+
+/**
+ * Created by Spicycurryman on 5/18/14.
+ */
+public class Select_Intensity_Activity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selectintensity);
+
+
+    }
+
+
+
+
+
+}
